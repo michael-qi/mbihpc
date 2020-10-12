4e1609940536405c0e69df1c03f4d501e53aa3aa
Qi Zhiguo <zq4@nyu.edu>
2020-10-12 14:11:30 +0800

    Create the mpijop script

---
+++ mpijob/mpijob.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -l walltime=24:00:00
+#PBS -l select=1:mem=24000mb:ncpus=36:ngpus=0:mpiprocs=36:cpu_arch=amd
+#PBS -N mpi_job
+#PBS -M qi20@nus.edu.sg
+#PBS -m abe
+#PBS -e log
+#PBS -o log
+#PBS -P hpc_common
+
+
+cd ~/mbihpc/mpijob
+module load intel
+mpirun -np 36 ./a.out
