ad084c2ccc55b942576c867a1ba878435b2f6dca
Qi Zhiguo <zq4@nyu.edu>
2020-10-12 14:44:02 +0800

    update gpuJob.sh

--- gpujob/gpuJob.sh
+++ gpujob/gpuJob.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#PBS -l walltime=24:00:00
-#PBS -l select=1:ncpus=1:ngpus=1:mem=2gb
-#PBS -N hello_Word
+#PBS -l walltime=1:00:00
+#PBS -l select=1:mem=400mb:ncpus=4:ngpus=1
+#PBS -N GPU_JOB
 #PBS -e log
-#PBS -m ae
 #PBS -o log
+#PBS -q testing
 #PBS -P hpc_common
 
-
-module load nvidia/nvhpc/20.7
-
+cd ~/mpihpc/gpujob
 hostname
-sleep 10
+nvidia-smi
+module purge
+module load nvidia/nvhpc/20.7
 hostname > `hostname`.txt
-echo $TMPDIR >> `hostname`.txt
-
+./gpu_burngcc 30 >> `hostname`.txt
